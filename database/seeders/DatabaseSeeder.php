a6a2c9811d614f96c4a1838926137089fbcab026
Luis Heras <joseluis@loomtek.mx>
2022-07-25 22:26:30 -0500

    Fecha_nacimiento factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(100)->create();
         \App\Models\UsuarioDomicilio::factory(100)->create();
 
     }
